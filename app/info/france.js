aad069b3a334b1f482b4e81ab639d20a8e3f4896
Minesh Mitha <Minesh_mitha@hotmail.com>
2015-02-12 17:36:03 +0000

    Added a storage + demo code
    Demo code is from week two workshop with fly controls disabled.
    Added a storage system, currently one country is in with very little
    information.
    Acts as a baseline for the rest.

---
+++ app/info/france.js
@@ -0,0 +1,48 @@
+Game.countryArray['France'] = {
+	active:false,
+	position:new THREE.Vector3(0,0,0),
+	model:'',
+	data:[]
+}
+
+Game.countryArray['France'].data[1939] = {
+	occupation:'French',
+	capital:'Paris',
+	events:[]
+}
+
+Game.countryArray['France'].data[1940] = {
+	occupation:'French',
+	capital:'Paris',
+	events:[]
+}
+
+Game.countryArray['France'].data[1941] = {
+	occupation:'French',
+	capital:'Paris',
+	events:[]
+}
+
+Game.countryArray['France'].data[1942] = {
+	occupation:'French',
+	capital:'Paris',
+	events:[]
+}
+
+Game.countryArray['France'].data[1943] = {
+	occupation:'French',
+	capital:'Paris',
+	events:[]
+}
+
+Game.countryArray['France'].data[1944] = {
+	occupation:'French',
+	capital:'Paris',
+	events:[]
+}
+
+Game.countryArray['France'].data[1945] = {
+	occupation:'French',
+	capital:'Paris',
+	events:[]
+}
--- app/js/main.js
+++ app/js/main.js
@@ -8,9 +8,14 @@ window.Game = {
 	renderer:'',
 	scene:'',
 	camera:'',
-
+	controls:'',
+	//-
+	countryArray:[],
+	now:0,
+	currentTime:new Date(),
+	clock:new THREE.Clock(),
 	objects:[],
-
+	rise:false,
 	init:function (){
 		console.log("Initiated.");
 		this.renderer = new THREE.WebGLRenderer();
@@ -24,48 +29,71 @@ window.Game = {
 
 		this.initCamera();
 		this.initScene();
-		render();
+		this.render();
 	},
 
 	initCamera:function(){
 
 		this.camera = new THREE.PerspectiveCamera(this.VIEW_ANGLE,this.ASPECT_RATIO,this.NEAR_CLIPPING_PLANE,this.FAR_CLIPPING_PLANE);
-		this.camera.position.set(0,0,300);
+		// 0.06636389772195989, _y: -0.8648781502759016, _z: -0.030489611766917137
+		// -591.0329190552981, y: -287.87343007596024, z: 936.1095241470686
+		this.camera.rotation.set(0.06636389772195989,-0.8648781502759016, -0.030489611766917137);
+		this.camera.position.set(-591,-287,936);
+		this.controls = new THREE.FlyControls( this.camera );
+		this.controls.movementSpeed = 100;
+		this.controls.domElement = document.getElementById('canvas-wrapper');
+		this.controls.rollSpeed = Math.PI/12;
+		this.controls.autoForward = false;
+		this.controls.dragToLook = true;
 	},
 
 	initScene:function(){
 
 		var radius = 100;
 		var segments = 16;
-		var rings = 16;
+		var rings = 3;
 		var sphereMaterial = new THREE.MeshLambertMaterial({
 			color: 0xFF0000
 		});
 
-
-
-		this.objects['sphere'] = new THREE.Mesh( new THREE.SphereGeometry(radius,segments,rings), sphereMaterial);
+		this.objects['sphere'] = new THREE.Mesh( new THREE.BoxGeometry(50,50,50), sphereMaterial);
 		this.objects['sphere'].position.y = 2;
 		this.scene.add(this.objects['sphere']);
 
 		this.objects['light'] = new THREE.PointLight(0xFFFFFF);
 		this.objects['light'].position.x = 10;
-		this.objects['light'].position.y = 20;
+		this.objects['light'].position.y = 180;
 		this.objects['light'].position.z = 130;
 
 		this.scene.add(this.objects['light']);
 
 	},
 
-	render:function(){
+	animate:function(deltaTime){
+			this.objects['sphere'].position.z += 10;
+	},
 
+	render:function(){
+		Game.renderer.render(Game.scene,Game.camera);
+		var deltaTime = Game.clock.getDelta();
+		// Game.controls.update(deltaTime);
+		Game.animate(deltaTime);
+		requestAnimationFrame(Game.render);
+	},
+
+	country:{
+		getByName:function(name){
+			return Game.countryArray[name];
+		},
+		getByNameYear:function(name,year){
+			return Game.countryArray[name].data[year];
+		}
 	}
 
 }
 
 function render(){
-		Game.renderer.render(Game.scene,Game.camera);
-		requestAnimationFrame(render);
+
 }
 
 
