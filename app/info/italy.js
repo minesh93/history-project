a0dda45db8b481a0747c9e21781e60af553c85e9
Minesh Mitha <Minesh_mitha@hotmail.com>
2015-03-03 17:00:11 +0000

    Added Elliot's Models and Positioned
    A working prototype has been made from this.

---
+++ app/info/italy.js
@@ -0,0 +1,43 @@
+Game.countryArray['Italy'] = new Country();
+
+Game.countryArray['Italy'].data[1939] = {
+	occupation:'Italian',
+	capital:'Rome',
+	events:[]
+}
+
+Game.countryArray['Italy'].data[1940] = {
+	occupation:'Italian',
+	capital:'Rome',
+	events:[]
+}
+
+Game.countryArray['Italy'].data[1941] = {
+	occupation:'Italian',
+	capital:'Rome',
+	events:[]
+}
+
+Game.countryArray['Italy'].data[1942] = {
+	occupation:'Italian',
+	capital:'Rome',
+	events:[]
+}
+
+Game.countryArray['Italy'].data[1943] = {
+	occupation:'Italian',
+	capital:'Rome',
+	events:[]
+}
+
+Game.countryArray['Italy'].data[1944] = {
+	occupation:'Italian',
+	capital:'Rome',
+	events:[]
+}
+
+Game.countryArray['Italy'].data[1945] = {
+	occupation:'Italian',
+	capital:'Rome',
+	events:[]
+}
